447bc437b5eb20f9820585f20404b699d8373e21
Alexis Sarmiento vs2022 <asarmiento@gmx.de>
2024-07-07 10:59:02 +0200

    added complex type model binding via json body

--- Chapter07_Model_Binding/SimpleTypes/Program.cs
+++ Chapter07_Model_Binding/SimpleTypes/Program.cs
@@ -7,7 +7,10 @@ app.MapGet("/", () => "Hello World!");
 
 //is supposed to be able to only interpret pxxxxx products with an integer
 //this only works if the Product implements a tryparse -> results in it being accepted as a simple type!
+//also added mandatory fromHeader to test a header read on the request
 app.MapGet("/product/{id}", (ProductId id, [FromHeader]string testHeader) => $"Testheader{testHeader} and received product with ID: {id}");
+
+app.MapPost("/JSONProduct", (JSONProduct product) => $"Product bound to body: {product}");
 
 app.Run();
 
@@ -28,3 +31,4 @@ readonly record struct ProductId(int Id)
         return false;
     } 
 }
+record JSONProduct(int Id, string Name, int Stock);
