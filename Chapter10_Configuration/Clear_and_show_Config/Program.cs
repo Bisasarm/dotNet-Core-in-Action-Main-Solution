5174ce780ece8f951d24a42219f5bdd20ee7ee54
Alexis Sarmiento vs2022 <asarmiento@gmx.de>
2024-07-11 17:22:02 +0200

    Added specific pair value retreival

--- Chapter10_Configuration/Clear_and_show_Config/Program.cs
+++ Chapter10_Configuration/Clear_and_show_Config/Program.cs
@@ -3,6 +3,8 @@ builder.Configuration.Sources.Clear();
 builder.Configuration.AddJsonFile("appsettings.json", optional: true);
 var app = builder.Build();
 
+//Retreiving all of the Config as Enumerable
 app.MapGet("/", () => app.Configuration.AsEnumerable());
-
+//Accessing a specific key value pair
+app.MapGet("/logginglevel", () => app.Configuration["Logging:LogLevel:Microsoft.AspNetCore"]);
 app.Run();
