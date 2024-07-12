2f760159892573b719e9a17fbd326d09a92789f4
Alexis Sarmiento vs2022 <asarmiento@gmx.de>
2024-07-12 22:51:38 +0200

    did not save changes

--- Chapter10_Configuration/Config_Multiple_Sources_and_Strong_Typing/Program.cs
+++ Chapter10_Configuration/Config_Multiple_Sources_and_Strong_Typing/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.Sources.Clear();
 //builder.Configuration.AddEnvironmentVariables();
+//This will set the information level to Warning
 builder.Configuration.AddJsonFile("appsettings.json");
+//This should overwrite the information level to Debug
 builder.Configuration.AddJsonFile("sharedsettings.json");
 if (builder.Environment.IsDevelopment())
 {
@@ -10,5 +14,14 @@ if (builder.Environment.IsDevelopment())
 var app = builder.Build();
 
 app.MapGet("/", () => app.Configuration.AsEnumerable());
+app.MapGet("/displaysettings", (IOptions<AppDisplaySettings> options) =>
+{
+
+});
+
+
 
 app.Run();
+internal class AppDisplaySettings
+{
+}
