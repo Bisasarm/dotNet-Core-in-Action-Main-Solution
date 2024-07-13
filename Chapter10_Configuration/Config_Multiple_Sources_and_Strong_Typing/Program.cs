4c0a36ff074e63622e0d787d4a224393cff3ad96
Alexis Sarmiento vs2022 <asarmiento@gmx.de>
2024-07-13 14:14:51 +0200

    added settings classes and injected them into minimal Endpoints

--- Chapter10_Configuration/Config_Multiple_Sources_and_Strong_Typing/Program.cs
+++ Chapter10_Configuration/Config_Multiple_Sources_and_Strong_Typing/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,9 @@ builder.Configuration.Sources.Clear();
 builder.Configuration.AddJsonFile("appsettings.json");
 //This should overwrite the information level to Debug
 builder.Configuration.AddJsonFile("sharedsettings.json");
+//injecting the Service for binded Configuration
+builder.Services.Configure<AppDisplaySettings>(builder.Configuration.GetSection(nameof(AppDisplaySettings)));
+builder.Services.Configure<MapSettings>(builder.Configuration.GetSection(nameof(MapSettings)));
 if (builder.Environment.IsDevelopment())
 {
     builder.Configuration.AddUserSecrets<Program>();
@@ -14,14 +18,37 @@ if (builder.Environment.IsDevelopment())
 var app = builder.Build();
 
 app.MapGet("/", () => app.Configuration.AsEnumerable());
-app.MapGet("/displaysettings", (IOptions<AppDisplaySettings> options) =>
+app.MapGet("/displaysettings", (IOptionsSnapshot<AppDisplaySettings> options) =>
 {
-
+    AppDisplaySettings settings = options.Value;
+    string title = settings.Title;
+    bool copyright = settings.ShowCopyright;
+    return new { title, copyright };
 });
-
+app.MapGet("/Mapsettings", (IOptionsSnapshot<MapSettings> options) =>
+{
+    MapSettings settings = options.Value;
+    int zoom = settings.DefaultZoomlevel;
+    DefaultLocation location = settings.DefaultLocation;
+    return new {zoom, location };
+});
 
 
 app.Run();
 internal class AppDisplaySettings
 {
+    public string Title { get; set; }
+    public bool ShowCopyright { get; set; }
+
+}
+internal class MapSettings
+{
+    public int DefaultZoomlevel { get; set; }
+    public DefaultLocation DefaultLocation { get; set; }
+}
+
+public class DefaultLocation
+{
+    public int Latitude { get; set; }
+    public int Longitude { get; set; }
 }
