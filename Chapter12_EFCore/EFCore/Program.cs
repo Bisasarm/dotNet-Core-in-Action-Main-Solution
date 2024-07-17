3f9f7a0daf4315896b582ee8999768bd2e946baf
Alexis Sarmiento vs2022 <asarmiento@gmx.de>
2024-07-17 11:41:16 +0200

    Added Data model and DBContext to registert the datamodel with. Also added DBContext as a service to the DI container

--- Chapter12_EFCore/EFCore/Program.cs
+++ Chapter12_EFCore/EFCore/Program.cs
@@ -1,6 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var connString = builder.Configuration.GetConnectionString("DefaultConnection");
+//adding the AppDbContext as a service to the DI Container
+//also adding the DBContextOptionsBuilder with the corresponding DB provider and the connection string
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(connString));
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public DbSet<Recipe> Recipes { get; set; }    
+}
+
+class Recipe
+{
+    public int RecipeId { get; set; }
+    public required string Name { get; set; }
+    public bool IsDeleted { get; set; }
+    public required string Method { get; set; }
+    public TimeSpan CookingTime { get; set; }
+
+    public required ICollection<Ingredient> Ingredients { get; set; }
+}
+
+class Ingredient
+{
+    public int IngredientId { get; set; }
+    public int RecipeId { get; set; }
+    public required string Name { get; set; }
+    public decimal Quantity { get; set; }
+    public required string Unit { get; set; }
+}
