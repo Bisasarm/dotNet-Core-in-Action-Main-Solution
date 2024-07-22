f1b22bfb33173ec2543ecc12919539d8f086a127
Alexis Sarmiento vs2022 <asarmiento@gmx.de>
2024-07-22 17:27:35 +0200

    WIP: Invokation of handlers does not work

---
+++ Chapter15_Razor_Routing_Handlers/Razor_Routing_Handlers/Pages/Privacy.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Razor_Routing_Handlers.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+        //public required string Argument { get; set; }
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+        public IActionResult OnPost()
+        {
+            //this should redirect to index on page
+            return RedirectToPage("/");
+        }
+        //public IActionResult OnPost(string test)
+        //{
+        //    Console.WriteLine(test);
+        //    return Page();
+        //}
+        //public void OnPut()
+        //{
+        //    Console.WriteLine("Argument");
+        //}
+    }
+
+}
