bbdf10d47cc04dafe40c2f4888eaf68040e855d7
Alexis Sarmiento vs2022 <asarmiento@gmx.de>
2024-07-27 14:17:11 +0200

    Added List to Forms in form of select tag helper

---
+++ Chapter18_Razor_TagHelpers/Forms_and_Tag_Helpers/Pages/CurrencyConverter.cshtml.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Forms_and_Tag_Helpers.Pages
+{
+    public class CurrencyConverterModel : PageModel
+    {
+        [BindProperty]
+        public InputModel? Input { get; set; }
+        public List<SelectListItem> CurrencyOut { get; set; }
+        public List<SelectListItem> CurrencyIn { get; set; }
+        public CurrencyConverterModel()
+        {
+            SelectListGroup OptGroupEuropean = new SelectListGroup { Name = "European" };
+            SelectListGroup OptGroupAsian = new SelectListGroup { Name = "Asian" };
+            SelectListGroup OptGroupAmerican = new SelectListGroup { Name = "American" };             
+            CurrencyIn = new List<SelectListItem>
+            {
+                    new SelectListItem{Value = "euro", Text = "€", Group=OptGroupEuropean},
+                    new SelectListItem{Value = "dollar", Text = "$", Group=OptGroupAmerican},
+                    new SelectListItem{Value = "yen", Text = "Y", Group=OptGroupAsian}
+            };
+            CurrencyOut = new List<SelectListItem>
+            {
+                    new SelectListItem{Value = "euro", Text = "€", Group=OptGroupEuropean},
+                    new SelectListItem{Value = "dollar", Text = "$", Group=OptGroupAmerican},
+                    new SelectListItem{Value = "yen", Text = "Y", Group=OptGroupAsian}
+            };
+        }
+        public void OnGet()
+        {
+        }
+        public class InputModel
+        {
+            public string SelectedValue1 { get; set; }
+            public string SelectedValue2 { get; set; }
+            //public IEnumerable<string> MultipleSelectedValue { get; set; }
+        }
+    }
+}
