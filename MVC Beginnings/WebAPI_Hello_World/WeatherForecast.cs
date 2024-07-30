47467a6dc1d7086b7aee741b5a84ca70f527dce9
Alexis Sarmiento vs2022 <asarmiento@gmx.de>
2024-07-30 13:56:54 +0200

    Initializing Web API project for debugging and analyzing the template

---
+++ MVC Beginnings/WebAPI_Hello_World/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace WebAPI_Hello_World
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
