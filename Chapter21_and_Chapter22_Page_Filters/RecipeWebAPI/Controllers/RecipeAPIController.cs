d4d6014d8eb2a07065156546aa6ee9d38e3b6d4b
Alexis Sarmiento vs2022 <asarmiento@gmx.de>
2024-08-03 15:39:38 +0200

    added apicontroller for recipe without filters bas a baseline for refactoring with filters

---
+++ Chapter21_and_Chapter22_Page_Filters/RecipeWebAPI/Controllers/RecipeAPIController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecipeWebAPI.Controllers
+{
+    public class RecipeAPIController : ControllerBase
+    {
+        private RecipeService _RecipeService { get; set; }
+        public RecipeAPIController(RecipeService recipeService)
+        {
+            _RecipeService = recipeService;
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest();
+                }
+                if (!_RecipeService.DoesRecipeExist(id))
+                {
+                    return NotFound();
+                }
+                return Ok(_RecipeService.GetRecipe(id));
+            }
+            catch (Exception ex)
+            {
+                return GetErrorResult(ex);
+            }
+
+        }
+        [HttpPost("{id}")]
+        public IActionResult Edit(int id)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest();
+                }
+                if (!_RecipeService.DoesRecipeExist(id))
+                {
+                    return NotFound();
+                }
+                //Exception to test out exception
+                //throw new Exception();
+                return Ok(_RecipeService.EditRecipe(id));
+            }
+            catch (Exception ex)
+            {
+                return GetErrorResult(ex);
+            }
+        }
+        private static IActionResult GetErrorResult(Exception ex)
+        {
+            var error = new ProblemDetails
+            {
+                Status = 500,
+                Title = "A custom error occured",
+                Detail = ex.Message,
+                Type = "https://httpstatuses.com/500"
+            };
+            return new ObjectResult(error);
+        }
+
+    }
+}
---
+++ Chapter21_and_Chapter22_Page_Filters/RecipeWebAPI/RecipeService.cs
@@ -0,0 +1,18 @@
+﻿namespace RecipeWebAPI
+{
+    public class RecipeService
+    {
+        public string GetRecipe(int id)
+        {
+            return $"Recipe No. {id}";
+        }
+        public string EditRecipe(int id)
+        {
+            return $"Edited Recipe No. {id}";
+        }
+        public bool DoesRecipeExist(int id)
+        {
+            return true;
+        }
+    }
+}
