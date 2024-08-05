d3f9ebc1526d32dbd125a34da73e2db949868c96
Alexis Sarmiento vs2022 <asarmiento@gmx.de>
2024-08-05 18:17:37 +0200

    Initialized Identity project. Initialized LocalDB. Changed SignInOptions

---
+++ Chapter23_Authentication/ASP_NET_CORE_Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP_NET_CORE_Identity.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
