158df310eb49bbf66188b2369866de1fc6fb2c68
Alexis Sarmiento vs2022 <asarmiento@gmx.de>
2024-08-10 10:43:53 +0200

    Creating the pages required for airport workflow

---
+++ Chapter24_Authorization/AirportAuthorization/Pages/Airport/AirportLounge.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AirportAuthorization.Pages.Airport
+{
+    public class AirportLoungeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Chapter24_Authorization/AirportAuthorization/Pages/Airport/AirportSecurity.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AirportAuthorization.Pages.Airport
+{
+    public class AirportSecurityModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
