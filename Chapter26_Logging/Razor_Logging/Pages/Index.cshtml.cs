c763229974a03284622e1d2327bbd943ab50256e
Alexis Sarmiento vs2022 <asarmiento@gmx.de>
2024-08-14 15:21:27 +0200

    Initialized logging project and injected logging framework interface into razorpage

---
+++ Chapter26_Logging/Razor_Logging/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Razor_Logging.Pages
+{
+    public class IndexModel : PageModel
+    {
+        //logger has already been established as property
+        private readonly ILogger<IndexModel> _logger;
+
+        //ILogger is expected to be injected via DI
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            _logger.LogInformation("Index page has been called via {Verb}", "get");
+        }
+    }
+}
